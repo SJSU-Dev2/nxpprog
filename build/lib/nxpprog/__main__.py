2856cef8c3ae72abc477309b68d2df23ac1984c3
Khalil Estell <kammcecorp@gmail.com>
2022-12-30 08:45:20 -0800

    :lipstick: Use RICH UI for progress bar

--- build/lib/nxpprog/__main__.py
+++
@@ -1,4 +0,0 @@
-from nxpprog import nxpprog
-import sys
-
-nxpprog.run_nxpprog(sys.argv)
--- src/nxpprog/__main__.py
+++ src/nxpprog/__main__.py
@@ -1,4 +1,3 @@
 from nxpprog import nxpprog
-import sys
 
-nxpprog.run_nxpprog(sys.argv)
+nxpprog.run_nxpprog()
